62a44fc2f23ae9fb6c2f212611dc0e146d0fb267
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-29 22:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const parseCode = (str) => {
-  // your code here
-};
+function parseCode(encodedString) {
+    // Split the string by zeros
+    const [firstName, lastName, id] = encodedString.split('0').filter(part => part !== '');
+    
+    // Return the result as an object
+    return { firstName, lastName, id };
+}
 
-// Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// Examples
+console.log(parseCode("John000Doe000123"));    // Output: { firstName: "John", lastName: "Doe", id: "123" }
+console.log(parseCode("michael0smith004331")); // Output: { firstName: "michael", lastName: "smith", id: "4331" }
+console.log(parseCode("Thomas00LEE0000043"));  // Output: { firstName: "Thomas", lastName: "LEE", id: "43" }
